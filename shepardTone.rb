c83c235c0f887495693320f660c79f628074dbf0
fuzzySi <lennanes@gmail.com>
2017-08-12 00:38:19 +0100

    Update shepardTone.rb

--- shepardTone.rb
+++ shepardTone.rb
@@ -1,55 +1,41 @@
-# Shepard tone
+# Shephard tone
 
+tempo = 120
 maxAmp = 1
-layers = 7
-use_synth
-notes = chord:c3, 'm7-5', num_octaves: 4
+layers = 6
+use_synth :pretty_bell
+notes = chord:c3, 'm9+5',  num_octaves: 4
+
+
 len = notes.length
-
-
 vols = []
-fade = len / 5 # proportion of loop affected by fade
+fade = len / 4 # proportion of loop affected by fade
 incr = maxAmp.to_f / fade   # or  # maxAmp.fdiv(fade)
-gaps = len / layers
 threads = []
 
 for i in 0...layers
-  threads[i] = gaps * i
-  #puts i, threads[i]
+  threads[i] = len / layers * i # sorts out where multiple ribbons are along scale
 end
-
-
-puts "length"
-puts len
-puts "fade"
-puts fade
-puts incr
-puts gaps
-
-for i in 0...fade
+for i in 0...fade # attribute fade volumes
   vols[i] = incr * (i + 1)
-  puts i, vols[i]
 end
 for i in fade...(len - fade)
   vols[i] = maxAmp
-  puts i, vols[i]
 end
 for i in (len - fade)...(len)
   vols[i] = incr * (len - i)
-  puts i, vols[i]
 end
 
 live_loop :scale do
-  use_bpm 120
-  # notes.each do |i|
+  use_bpm tempo
   for i in 0...len
     for j in 0...layers
       k = i + threads[j]
       if k >= len then
         k = k - len
       end
-      puts notes[k], vols[k]
-      play notes[k], amp: vols[k]
+      puts notes[k], vols[k], (k.fdiv(len)* 2)-1
+      play notes[k], amp: vols[k], attack: 0.5, release: 1.5, pan: (k.fdiv(len)* 2)-1
     end
     sleep 0.5
   end
