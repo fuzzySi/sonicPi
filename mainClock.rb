1783bbe6b500902441ed23db36176cfd9959b3d5
fuzzySi <lennanes@gmail.com>
2020-08-24 00:11:35 +0100

    Update mainClock.rb

--- mainClock.rb
+++ mainClock.rb
@@ -1,43 +1,36 @@
 ## master clock
 
-# change me
 bpm = 120
-quaversInLoop = 16 # quavers before loop retriggered
+quaversInLoop = 16
 
 
-
-# system vars etc
+# system etc
 use_osc "localhost", 4560
 use_midi_defaults port: "usb_ms1x1_midi_interface_midi_1", channel: "10", sustain: 0.1
-# "usb_midi_4i4o_midi_1"
 use_debug false
-use_osc_logging true # false
+use_osc_logging false
+use_cue_logging false
+use_midi_logging false
 
 
-## master OSC loop
+
+## master OSC loops
+
 live_loop :clock do
-  # use_real_time
   use_bpm bpm
-  # midi_clock_beat
-  # n = 0
   osc "/start"
   midi_start
   quaversInLoop.times do |n|
-    puts n
+    puts "beat", n
     osc "/quaver", n
-    
     if n % 4 == 0 # tick every 4 clocks
       osc "/tick"
-      puts "4th beat"
-      midi :as2
     end
     sleep 0.25
-    #  n += 1
   end
 end
 
 # convert osc to MIDI
-
 live_loop :clockOut do
   use_real_time
   sync  "/osc*/tick"
